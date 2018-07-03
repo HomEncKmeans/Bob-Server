e56d63c5c60dd1b1a17cd686da23304a3aba20ee
George <geosakel@uom.edu.gr>
2018-07-03 08:59:05 +0300

    t2v1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 //#include "UServerT1V1.h"
-#include "UServerT1V2.h"
+//#include "UServerT1V2.h"
 //#include "UServerT1V3.h"
-
-//#include "UServerT2V2.h"
+//#include "UServerT2V1.h"
+#include "UServerT2V2.h"
 //#include "UServerT2V3.h"
 
 
@@ -15,15 +15,11 @@ int main() {
     auto t_start = chrono::high_resolution_clock::now();
 
     //UServerT1V1 server("127.0.0.1",5001,"127.0.0.1",5002,3);
-    UServerT1V2 server("127.0.0.1",5001,"127.0.0.1",5002,3);
-
+    // UServerT1V2 server("127.0.0.1",5001,"127.0.0.1",5002,3);
     //UServerT1V3 server("127.0.0.1",5001,"127.0.0.1",5002,3);
-
+    //UServerT2V1 server("127.0.0.1",5001,"127.0.0.1",5002,3);
+    UServerT2V2 server("127.0.0.1",5001,"127.0.0.1",5002,3);
     //UServerT2V3 server("127.0.0.1",5001,"127.0.0.1",5002,3);
-
-
-
-    //UServerT1V2 server("127.0.0.1",5001,3);
 
     std::clock_t c_end = std::clock();
     auto t_end = std::chrono::high_resolution_clock::now();
